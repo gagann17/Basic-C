be1ced2c0ffec74e41de0ec5edd3ec1ceddd9c30
gagann17 <43664966+gagann17@users.noreply.github.com>
2018-09-28 13:13:49 +0530

    Create codeday1.c

---
+++ codeday1.c
@@ -0,0 +1,17 @@
+DEMO
+#include<stdio.h>
+void main()
+{
+printf("\n Hello world\n");
+}
+
+
+ACTVITY
+#include<stdio.h>
+void main()
+{
+char s[10];
+printf("\n Enter your name\n");
+scanf("%s",s);
+printf("My name is %s",s);
+}
